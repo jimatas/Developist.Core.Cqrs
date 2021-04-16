af74be66ce35c593d02c7fabad1b39065188d0fb
jimatas <atask@hr.nl>
2021-04-16 08:40:07 +0200

    Unit test project added.

---
+++ Developist.Core.Cqrs.Tests/Fixture/Message.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2021 Jim Atas. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for details.
+
+using System;
+
+namespace Developist.Core.Cqrs.Tests.Fixture
+{
+    public class Message
+    {
+        public Message() { }
+        public Message(Guid id) => Id = id;
+
+        public Guid Id { get; }
+        public string Text { get; set; }
+
+        public override string ToString() => Text;
+    }
+}
---
+++ Developist.Core.Cqrs.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2021 Jim Atas. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for details.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+namespace Developist.Core.Cqrs.Tests
+{
+    [TestClass]
+    public class ServiceCollectionExtensionsTests
+    {
+        private IServiceProvider serviceProvider;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var services = new ServiceCollection();
+            services.AddCqrs();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        [TestMethod]
+        public void AddCqrs_CalledOnCurrentAssembly_RegistersDispatchers()
+        {
+            // Arrange
+
+            // Act
+            var dispatcher = serviceProvider.GetService<IDispatcher>();
+            var commandDispatcher = serviceProvider.GetService<ICommandDispatcher>();
+            var queryDispatcher = serviceProvider.GetService<IQueryDispatcher>();
+            var eventDispatcher = serviceProvider.GetService<IEventDispatcher>();
+
+            // Assert
+            Assert.IsNotNull(dispatcher);
+            Assert.IsNotNull(commandDispatcher);
+            Assert.IsNotNull(queryDispatcher);
+            Assert.IsNotNull(eventDispatcher);
+        }
+
+        [TestMethod]
+        public void AddCqrs_CalledOnCurrentAssembly_RegistersDispatcherSuperTypesAsSelf()
+        {
+            // Arrange
+
+            // Act
+            var dispatcher = serviceProvider.GetService<IDispatcher>();
+            var commandDispatcher = serviceProvider.GetService<ICommandDispatcher>();
+            var queryDispatcher = serviceProvider.GetService<IQueryDispatcher>();
+            var eventDispatcher = serviceProvider.GetService<IEventDispatcher>();
+
+            // Assert
+            Assert.AreEqual(dispatcher, commandDispatcher);
+            Assert.AreEqual(dispatcher, queryDispatcher);
+            Assert.AreEqual(dispatcher, eventDispatcher);
+        }
+    }
+}
